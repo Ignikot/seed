4089c375d9722bb2d7275be9139a38b34299a77f
ink_mail@yahoo.com <inkpassword1>
2013-04-29 08:47:15 +0400

    new seed project

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('seedApp', ['seedApp.filters', 'seedApp.services', 'seedApp.directives', 'ui.bootstrap']).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: SeedCtrl1});
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }]);
---
+++ app/js/controllers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* Controllers */
+
+
+function SeedCtrl1() {}
+SeedCtrl1.$inject = [];
+
+
+function MyCtrl2() {
+}
+MyCtrl2.$inject = [];
