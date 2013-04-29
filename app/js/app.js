a8bd87837ea690528c32ddc602edf27602c50770
Igor Kotenko <ink_mail@yahoo.com>
2013-04-29 17:48:42 +0400

    разработка примера

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('seedApp', ['seedApp.filters', 'seedApp.services', 'seedApp.directives', 'ui.bootstrap']).
+angular.module('seedApp', ['seedApp.filters', 'seedApp.services', 'seedApp.directives', 'ui.bootstrap', 'ngGrid']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: SeedCtrl1});
     $routeProvider.otherwise({redirectTo: '/view1'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,36 @@
 
 
 function SeedCtrl1() {}
-SeedCtrl1.$inject = [];
 
 
 function MyCtrl2() {
 }
-MyCtrl2.$inject = [];
+
+function AppInputCtrl($scope) {
+    $scope.application = {
+        number: '000000001',
+        regdate: '01/01/2013',
+        decisionterm:'',
+        product: 'А/м с пробегом',
+        salespoint: 'Точка продаж',
+        sum: 100
+    };
+
+    $scope.myData = [
+        {name: "Иванов Иван Иванович", age: 43},
+        {name: "Иванов Андрей Иванович", age: 43},
+        {name: "Петров Петр Петрович", age: 27}
+    ];
+    $scope.gridOptions = { data: 'myData',
+        columnDefs: [
+            {field:'name', displayName:'Имя'},
+            {field:'age', displayName:'Возраст'}
+        ]};
+
+    $scope.calc = function() {
+        $scope.myData = [
+            {name: "Петров Петр Петрович", age: 27}
+        ];
+    }
+
+}
